05d4159085dbdcf2f39fb31869d168451d5a1ebe
Oliver Anhuth <oliver.anhuth@web.de>
2021-06-11 09:21:32 +0200

    Use single CDN provider.

--- react/main.tsx
+++ react/main.tsx
@@ -16,7 +16,7 @@ const html = `
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css">
+    <link rel="stylesheet" href="https://esm.sh/purecss@2.0.3/build/pure-min.css">
     <title>Document</title>
 </head>
   <body>
