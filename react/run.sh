bc3ae987eeafc86fc56c48ef42b0b4b8402db7bb
Oliver Anhuth <oliver.anhuth@web.de>
2021-06-11 16:46:25 +0200

    Use separate lock file for server and application.

--- react/run.sh
+++ react/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if deno bundle --import-map import_map.json --lock lock.json app.tsx app.js; then
-    exec deno run --import-map import_map.json --lock lock.json --allow-read --allow-env --allow-net serve.ts "$@"
+if deno bundle --import-map import_map.json --lock app.lock.json app.tsx app.js; then
+    exec deno run --import-map import_map.json --lock serve.lock.json --allow-read --allow-env --allow-net serve.ts "$@"
 fi
