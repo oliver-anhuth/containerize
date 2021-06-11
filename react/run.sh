458428ddab003106fcede8ec292761a200275736
Oliver Anhuth <oliver.anhuth@web.de>
2021-06-11 14:55:21 +0200

    Implement client side application bundling.

--- react/run.sh
+++ react/run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec deno run --import-map import_map.json --lock lock.json --allow-env --allow-net main.tsx "$@"
+if deno bundle --import-map import_map.json --lock lock.json app.tsx app.js; then
+    exec deno run --import-map import_map.json --lock lock.json --allow-read --allow-env --allow-net serve.ts "$@"
+fi
