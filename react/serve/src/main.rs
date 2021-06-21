69e7c2914d572ae56ddb8d54131299849988ca14
Oliver Anhuth <oliver.anhuth@web.de>
2021-06-21 23:00:01 +0200

    Add status message.

--- react/serve/src/main.rs
+++ react/serve/src/main.rs
@@ -2,6 +2,7 @@ use warp::{self, Filter};
 
 #[tokio::main]
 async fn main() {
+    let port = 8080;
     let html = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("app.html"));
@@ -18,5 +19,6 @@ async fn main() {
         .and(warp::path::end())
         .and(warp::fs::file("app.svg"));
     let routes = html.or(js).or(css).or(favicon);
-    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+    println!("Listening on http://localhost:{}", port);
+    warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
